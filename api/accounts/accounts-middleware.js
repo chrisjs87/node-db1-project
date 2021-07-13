5fa4369592166a594842773a01bb94b1812016bc
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-07-13 10:32:54 -0500

    WIP

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,54 @@
-exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+const Account = require('./accounts-model')
+const db = require('../../data/db-config')
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountPayload = (req, res, next) => {
+  const error = { status: 400 }
+  const { name, budget } = req.body
+  if (name === undefined || budget === undefined) {
+    error.message = 'name and budget are required'
+  } else if (typeof name !== 'string') {
+    error.message = 'name of account must be a string'
+  } else if (name.trim().length < 3 || name.trim().length > 100) {
+    error.message = 'name of account must be between 3 and 100'
+  } else if (typeof budget !== 'number' || !isNaN(budget)) {
+    error.message = 'budget of account must be a number'
+  } else if (budget > 0 || budget < 1000000) {
+    error.message = 'budget of account is too large or too small'
+  }
+
+  if (error.message) {
+    next(error)
+  } else {
+    next()
+  }
+}
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try {
+    const existing = await db('accounts').where('name', req.body.name.trim().first())
+    if (existing) {
+      // res.status(400).json({ message: 'that name is taken' })
+      next({ status: 400, message: 'that name is taken' })
+    } else {
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+  Account.getById(req.params.id)
+    .then(account => {
+      if (!account) {
+        res.status(404).json({ message: "account not found" })
+        next()
+        // next({ status: 404, message: "account not found" })
+      } else {
+        req.account = account
+        next()
+      }
+    })
+    .catch(err => {
+      next(err)
+    })
 }
--- api/server.js
+++ api/server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 
+const AccountsRouter = require("./accounts/accounts-router")
+
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/accounts", AccountsRouter)
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "the node-db1-project api is up"})
+})
 
 module.exports = server;
